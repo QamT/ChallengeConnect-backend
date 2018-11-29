71ee4e0ff7267bce7d8451b4f6e3fd4ff9b7669f
Qamar Idrees <qam.idrees@gmail.com>
2018-11-29 14:01:18 -0600

    update

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   PORT: process.env.PORT || 3000,
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000',
+  CLIENT_ORIGIN: 'localhost:3000', //process.env.CLIENT_ORIGIN || 
   DATABASE_URL: process.env.DATABASE_URL || 'mongodb://localhost/react-app',
   TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'mongodb://localhost/test-react-app',
   JWT_SECRET: process.env.JWT_SECRET || 'secret',
